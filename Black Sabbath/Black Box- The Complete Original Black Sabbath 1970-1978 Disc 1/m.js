4c250a9bac2cb7fc71bdec0cd1668324357417fd
littleflute <13699175164@sina.cn>
2017-07-13 14:42:02 -0500

    Add files via upload

---
+++ Black Sabbath/Black Box- The Complete Original Black Sabbath 1970-1978 Disc 1/m.js
@@ -0,0 +1,8 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Black Sabbath.mp3";		
+f[2]	= "02 The Wizard.mp3";		
+f[3]	= "03 Wasp-Behind the Wall of Sleep-Bassically-N.I.B.mp3";		
+f[4]	= "04 Track 4.mp3";		
+f[5]	= "05 Wicked World.mp3";		
+f[6]	= "06 Track 6.mp3";		
