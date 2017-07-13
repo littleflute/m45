c3bf749444c3c05acb48f55f8b98d43ca98d7059
littleflute <13699175164@sina.cn>
2017-07-13 14:49:49 -0500

    Add files via upload

---
+++ The Kinks/Something Else by the Kinks/m.js
@@ -0,0 +1,15 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 David Watts.mp3";		
+f[2]	= "02 Death of a Clown.mp3";		
+f[3]	= "03 Two Sisters.mp3";		
+f[4]	= "04 No Return.mp3";		
+f[5]	= "05 Harry Rag.mp3";		
+f[6]	= "06 Tin Soldier Man.mp3";		
+f[7]	= "07 Situation Vacant.mp3";		
+f[8]	= "08 Love Me Till the Sun Shines.mp3";		
+f[9]	= "09 Lazy Old Sun.mp3";		
+f[10]	= "10 Afternoon Tea.mp3";		
+f[11]	= "11 Funny Face.mp3";		
+f[12]	= "12 End of the Season.mp3";		
+f[13]	= "13 Waterloo Sunset.mp3";		
